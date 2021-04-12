ac74329f827eea1b433e9b5f8627532d191dc2bb
Frank Terragna <fterragna@gmail.com>
2021-04-11 21:09:44 -0400

    Tests for expressions

---
+++ expression_test.go
@@ -0,0 +1,97 @@
+package milo
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAnd(t *testing.T) {
+	assert := assert.New(t)
+
+	exprs := []Expression{
+		expr{
+			field:   "foo",
+			operand: OpEqual,
+			value:   "bar",
+		},
+		expr{
+			field:   "bar",
+			operand: OpNotEqual,
+			value:   "baz",
+		},
+	}
+
+	actual := And(exprs...)
+
+	assert.Equal(exprList{t: ExprTypeAnd, exprs: exprs}, actual)
+}
+
+func TestOr(t *testing.T) {
+	assert := assert.New(t)
+
+	exprs := []Expression{
+		expr{
+			field:   "foo",
+			operand: OpEqual,
+			value:   "bar",
+		},
+		expr{
+			field:   "bar",
+			operand: OpNotEqual,
+			value:   "baz",
+		},
+	}
+
+	actual := Or(exprs...)
+
+	assert.Equal(exprList{t: ExprTypeOr, exprs: exprs}, actual)
+}
+
+func TestEqual(t *testing.T) {
+	assert := assert.New(t)
+
+	actual := Equal("foo", "bar")
+
+	assert.Equal(expr{field: "foo", operand: OpEqual, value: "bar"}, actual)
+}
+
+func TestNotEqual(t *testing.T) {
+	assert := assert.New(t)
+
+	actual := NotEqual("foo", "bar")
+
+	assert.Equal(expr{field: "foo", operand: OpNotEqual, value: "bar"}, actual)
+}
+
+func TestGt(t *testing.T) {
+	assert := assert.New(t)
+
+	actual := Gt("foo", "bar")
+
+	assert.Equal(expr{field: "foo", operand: OpGt, value: "bar"}, actual)
+}
+
+func TestLt(t *testing.T) {
+	assert := assert.New(t)
+
+	actual := Lt("foo", "bar")
+
+	assert.Equal(expr{field: "foo", operand: OpLt, value: "bar"}, actual)
+}
+
+func TestGte(t *testing.T) {
+	assert := assert.New(t)
+
+	actual := Gte("foo", "bar")
+
+	assert.Equal(expr{field: "foo", operand: OpGte, value: "bar"}, actual)
+}
+
+func TestLte(t *testing.T) {
+	assert := assert.New(t)
+
+	actual := Lte("foo", "bar")
+
+	assert.Equal(expr{field: "foo", operand: OpLte, value: "bar"}, actual)
+}
