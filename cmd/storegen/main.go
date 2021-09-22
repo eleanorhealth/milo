e8b6283a9a0de99db8b150b443c5d933cd1fa694
Frank Terragna <fterragna@gmail.com>
2021-09-22 14:05:13 -0400

    Added support for not found error types when generating stores

--- cmd/storegen/main.go
+++ cmd/storegen/main.go
@@ -13,21 +13,23 @@ import (
 var tpl string
 
 type data struct {
-	EntityName string
-	EntityType string
-	IDType     string
+	EntityName        string
+	EntityType        string
+	IDType            string
+	NotFoundErrorType string
 }
 
 func main() {
-	var entityName, entityType, idType string
+	var entityName, entityType, idType, notFoundErrorType string
 
 	flag.StringVar(&entityName, "entityName", "", "domain entity name (e.g., Customer)")
 	flag.StringVar(&entityType, "entityType", "", "domain entity type (e.g., *domain.Customer)")
 	flag.StringVar(&idType, "idType", "", "domain ID type (e.g., entityid.ID)")
+	flag.StringVar(&notFoundErrorType, "notFoundErrorType", "", "entity not found error type (e.g., domain.ErrNotFound)")
 
 	flag.Parse()
 
-	if len(entityName) == 0 || len(entityType) == 0 || len(idType) == 0 {
+	if len(entityName) == 0 || len(entityType) == 0 || len(idType) == 0 || len(notFoundErrorType) == 0 {
 		flag.PrintDefaults()
 		os.Exit(0)
 	}
@@ -40,9 +42,10 @@ func main() {
 	}
 
 	d := data{
-		EntityName: entityName,
-		EntityType: entityType,
-		IDType:     idType,
+		EntityName:        entityName,
+		EntityType:        entityType,
+		IDType:            idType,
+		NotFoundErrorType: notFoundErrorType,
 	}
 
 	err = t.Execute(os.Stdout, d)
